ea554015a3e477a50f6898ad46198f80df15ebe9
Piyush Sachan <piyush.fgk@gmail.com>
2023-11-29 19:09:35 +0600

    Starting Project Display Learning Resources Component

---
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: { LearningResource },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 1,
+          title: 'Official Vue JS',
+          description: 'Learn Vue JS',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 2,
+          title: 'Google Search',
+          description: 'Search google for your studies',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
