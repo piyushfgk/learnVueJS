d892c12da4b50d1303d2236625fdbcc2f837ea27
Piyush Sachan <piyush.fgk@gmail.com>
2023-12-04 18:46:28 +0600

    Form added to enter new resource

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,3 +1,64 @@
 <template>
-    <h2>I am resources</h2>
+    <base-card>
+        <form @submit.prevent="submitData">
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" ref="titleInput">
+            </div>
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="3" ref="descriptionInput"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input type="url" name="link" id="link" ref="linkInput">
+            </div>
+            <div>
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+        </form>
+    </base-card>
 </template>
+
+<script>
+export default {
+    inject: ['addResource'],
+    methods: {
+        submitData() {
+            const enteredTitle = this.$refs.titleInput.value
+            const enteredDescription = this.$refs.descriptionInput.value
+            const enteredUrl = this.$refs.linkInput.value
+
+            this.addResource(enteredTitle, enteredDescription, enteredUrl)
+        }
+    },
+}
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <base-card>
-      <base-button @click="setSelectedTab('stored-resources')"
+      <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode"
         >Stored Resources</base-button
       >
-      <base-button @click="selectedTab('add-resource')"
+      <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode"
         >Add Resource</base-button
       >
     </base-card>
@@ -13,6 +13,17 @@
 </template>
 
 <script>
+
+function getMaxIdPlusOne(resources) {
+  // Use Array.reduce to find the maximum id
+  const maxId = resources.reduce((max, resource) => {
+    return resource.id > max ? resource.id : max;
+  }, 0);
+
+  // Return maxId + 1
+  return maxId + 1;
+}
+
 import StoredResources from './StoredResources.vue';
 import AddResource from './AddResource.vue';
 
@@ -37,15 +48,35 @@ export default {
       ],
     };
   },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat'
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat'
+    }
+  },
   provide() {
     return {
-        resources: this.storedResources
+        resources: this.storedResources,
+        addResource: this.addResource
     }
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
     },
+    addResource(title, description, url) {
+      const newResource = {
+        id: getMaxIdPlusOne(this.storedResources),
+        title: title,
+        description: description,
+        link: url
+      }
+
+      this.storedResources.unshift(newResource)
+      this.selectedTab = 'stored-resources'
+    }
   },
 };
 </script>
