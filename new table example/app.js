6d1aa15c1d4706d6a4eb90603bea37e23369ee81
Piyush Sachan <piyush.fgk@gmail.com>
2023-12-07 09:42:45 +0600

    Old tables examples

--- new table example/app.js
+++ new table example/app.js
@@ -1,11 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-
+            selectedMisCsvr: [],
+            serialNos: [],
+            selectedQuantities: []
         }
     },
-    mounted() {
-        console.log("App mounted!")
+    methods: {
+        updateSerialList(index) {
+              const [itemNumber, quantity] = selectedMisCsvr[index].split("|");
+
+            console.log({
+                itemNumber: itemNumber,
+                quantity: quantity,
+                selectedRow: index
+            });
+            // Generate the list of serial numbers
+            // const serialList = [];
+            // for (let i = 1; i <= quantity; i++) {
+            //     const optionValue = itemNumber + '|' + i;
+            //     if (!this.usedSerialNumbers[index].includes(optionValue)) {
+            //         serialList.push({ label: itemNumber, value: optionValue });
+            //     }
+            // }
+
+            // // Update the Serial No. column
+            // this.$set(this.serialNumbers, index, serialList);
+        }
     },
 })
 
