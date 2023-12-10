6ef2f708e9f0e5db6c6f4e3ffa1e14a63c3d8be5
Piyush Sachan <piyush.fgk@gmail.com>
2023-12-11 05:38:08 +0600

    Http handle data loading cases using if conditions

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -8,7 +8,8 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found. Start adding some survey first.</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
