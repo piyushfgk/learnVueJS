0a12ce30ef1709c99507c32607f752493308d9d6
Piyush Sachan <piyush.fgk@gmail.com>
2023-12-28 06:22:25 +0600

    Creating alias for a route

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/main.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/main.js
@@ -9,7 +9,7 @@ import TeamMembers from './components/teams/TeamMembers.vue'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/teams', component: TeamsList },
+        { path: '/teams', component: TeamsList, alias: '/' },
         { path: '/users', component: UsersList },
         { path: '/teams/:teamId', component: TeamMembers, props: true },
     ],
