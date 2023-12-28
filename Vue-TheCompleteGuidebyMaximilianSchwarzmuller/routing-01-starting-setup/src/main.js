46758f44cf5f68efa49e70d185186aa3690cd0b1
Piyush Sachan <piyush.fgk@gmail.com>
2023-12-28 06:46:33 +0600

    Instead of adding direct routes for child components, now using child routes

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/main.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/main.js
@@ -2,21 +2,26 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue'
-import UsersList from './components/users/UsersList.vue'
-import TeamMembers from './components/teams/TeamMembers.vue'
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/teams' },
-        { path: '/teams', component: TeamsList },
-        { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers, props: true },
-        { path: '/:notFound(.*)', component: NotFound }
-    ],
-    linkActiveClass: 'active'
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      path: '/teams',
+      component: TeamsList,
+      children: [
+        { path: ':teamId', component: TeamMembers, props: true }
+        ],
+    },
+    { path: '/users', component: UsersList },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+  linkActiveClass: 'active',
 });
 
 const app = createApp(App);
