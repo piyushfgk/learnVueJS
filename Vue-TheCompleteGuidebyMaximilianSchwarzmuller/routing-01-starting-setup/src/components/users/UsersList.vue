b9e3d701101f7ec63b70d707ab56ae97f738aa18
Piyush Sachan <piyush.fgk@gmail.com>
2024-01-02 06:04:42 +0600

    Action before you leave a component, like leaving page without saving

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/components/users/UsersList.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="goToPage">Go to Teams</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,15 +14,38 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      formSaved: false,
+    }
+  },
   methods: {
     goToPage() {
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.formSaved = true;
     }
   },
   beforeRouteEnter (to, from, next) {
     console.error("Before route enter UsersList component");
     console.log({to: to, from: from});
     next();
+  },
+  unmounted() {
+    console.log("Unmounted");
+  },
+  beforeRouteLeave (to, from, next) {
+    if(this.formSaved === true) {
+      next();
+    } else {
+      const userConfirm = confirm("Changes have been saved yet! Are you sure want to leave?");
+
+      console.log("Before Users List Component Leave");
+      console.log({to:to,from:from});
+
+      next(userConfirm);
+    }
   }
 };
 </script>
