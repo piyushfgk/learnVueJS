ee6023392e4204e5a0ae709d0a797b160e052d48
Piyush Sachan <piyush.fgk@gmail.com>
2024-01-09 06:56:56 +0600

    Adding transitions to multiple elment - transition-group component

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/animations-01-starting-setup/src/components/UsersList.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/animations-01-starting-setup/src/components/UsersList.vue
@@ -1,7 +1,7 @@
 <template>
-  <ul>
+  <transition-group tag="ul" name="users-list">
     <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-  </ul>
+  </transition-group>
   <input type="text" ref="inputUser" />
   <button @click="addUser">Add User</button>
 </template>
@@ -39,5 +39,28 @@ li{
     border: 1px solid #eee;
     text-align: center;
     cursor: pointer;
+}
+
+.users-list-enter-from{
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.users-list-enter-active{
+    transition: all 0.5s ease-out;
+}
+
+.users-list-enter-to, .users-list-leave-from{
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.users-list-leave-active{
+    transition: all 0.5s ease-in;
+}
+
+.users-list-leave-to{
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
