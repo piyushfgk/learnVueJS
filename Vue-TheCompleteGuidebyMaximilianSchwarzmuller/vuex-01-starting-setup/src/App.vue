18074a4b9c65eff8a347c16a3bc5a1c7555278b3
Piyush Sachan <piyush.fgk@gmail.com>
2024-01-11 06:09:24 +0600

    Basic Vuex Example - Import and change variable state

---
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ counter }}</h3>
+    <button @click="addToCounter">Add</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    addToCounter() {
+      this.$store.state.counter++;
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
