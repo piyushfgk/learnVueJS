40f1c8e1b818683dc1b57914c8be2c9bc4f726b6
Piyush Sachan <piyush.fgk@gmail.com>
2024-01-12 06:12:34 +0600

    How to use existing getter value inside another getter function

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/vuex-01-starting-setup/src/main.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/vuex-01-starting-setup/src/main.js
@@ -20,6 +20,14 @@ const store = createStore({
     getters: {
         finalCounter(state) {
             return state.counter * 2;
+        },
+        normaliseCounter(_, getters) {
+            const finalCounter = getters.finalCounter;
+
+            if(finalCounter < 0) return 0;
+            if(finalCounter > 100) return 100;
+
+            return finalCounter;
         }
     }
 });
