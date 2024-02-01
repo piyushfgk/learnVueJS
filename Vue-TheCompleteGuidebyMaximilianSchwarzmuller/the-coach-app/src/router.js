9a61a25b7e0e760725c0ab2b369e286d92cc487f
Piyush Sachan <piyush.fgk@gmail.com>
2024-02-01 06:38:09 +0600

    The coach app starting project with routing defined

---
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: null },
+    { path: '/coaches/:id', component: null, children: [{ path: '/contact', component: null }] },
+    { path: '/register', component: null },
+    { path: '/contact', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', component: null }
+  ]
+})
+
+export default router
