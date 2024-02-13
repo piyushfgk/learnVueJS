bbdb99437ed5bdb3eba2d98656c7eccb069d05cf
Piyush Sachan <piyush.fgk@gmail.com>
2024-02-13 06:16:07 +0600

    View coach details page

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/router.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/router.js
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegister from './pages/coaches/CoachRegister.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachesList from './pages/coaches/CoachesList.vue'
+import CoachRegister from './pages/coaches/CoachRegister.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
 import RequestReceived from './pages/requests/RequestReceived.vue'
-import NotFound from './pages/NotFound.vue';
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
-    { path: '/coaches/:id', component: CoachDetail, children: [{ path: 'contact', component: ContactCoach }] },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      props: true,
+      children: [{ path: 'contact', component: ContactCoach }]
+    },
     { path: '/register', component: CoachRegister },
     { path: '/requests', component: RequestReceived },
     { path: '/:notFound(.*)', component: NotFound }
