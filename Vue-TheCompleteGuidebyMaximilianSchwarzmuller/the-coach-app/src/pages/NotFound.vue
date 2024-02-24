a37e94865fc82165c320c057d97360ab9bd9d406
Piyush Sachan <piyush.fgk@gmail.com>
2024-02-24 06:19:36 +0600

    Adding page not found

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/pages/NotFound.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-    <h3>Not found...</h3>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found — maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
