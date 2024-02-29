1e0c20fc037db0008bf103ee0ad6dfb5a3b9e1b7
Piyush Sachan <piyush.fgk@gmail.com>
2024-02-29 07:43:25 +0600

    Now register as a coach after login. Authentication required to register

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/store/modules/coaches/actions.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/store/modules/coaches/actions.js
@@ -9,8 +9,10 @@ export default {
       areas: data.areas
     }
 
+    const token = context.rootGetters.token
+
     const response = await fetch(
-      `https://vue-http-demo-c7025-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://vue-http-demo-c7025-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json?auth=${token}`,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/store/modules/requests/actions.js
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/the-coach-app/src/store/modules/requests/actions.js
@@ -28,8 +28,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId
+    const token = context.rootGetters.token
+
     const response = await fetch(
-      `https://vue-http-demo-c7025-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json`
+      `https://vue-http-demo-c7025-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json?auth=${token}`
     )
     const responseData = await response.json()
 
