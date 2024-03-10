7db0bdd46df02c6fe0a6e47ee9a9877b688b640e
Piyush Sachan <piyush.fgk@gmail.com>
2024-03-11 05:59:30 +0600

    Passing and using setup method in another component

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/composition-01-starting-setup/src/components/UserData.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/composition-01-starting-setup/src/components/UserData.vue
@@ -5,13 +5,16 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ['firstName', 'lastName', 'age'],
-  computed: {
-    fullName() {
-      return this.firstName + ' ' + this.lastName;
-    },
-  },
-};
+<script setup>
+import { computed, defineProps } from 'vue';
+
+// const props = defineProps(['firstName', 'lastName']);
+const props = defineProps({
+  firstName: String,
+  lastName: String,
+});
+
+const fullName = computed(function () {
+  return props.firstName + ' ' + props.lastName;
+});
 </script>
