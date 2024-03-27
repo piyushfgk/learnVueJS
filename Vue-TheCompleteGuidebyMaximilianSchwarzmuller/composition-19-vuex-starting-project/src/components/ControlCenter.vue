bfa7052d5e96a6c7ac9a14a8be5839c0a2351e90
Piyush Sachan <piyush.fgk@gmail.com>
2024-03-27 06:16:19 +0600

    Using store in setup. Also called vuex

---
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/composition-19-vuex-starting-project/src/components/ControlCenter.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script setup>
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+function inc() {
+  store.dispatch('increment');
+}
+</script>
---
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/composition-19-vuex-starting-project/src/components/TheCounter.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>{{ counter }}</h2>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const counter = computed(() => store.getters.counter);
+</script>
