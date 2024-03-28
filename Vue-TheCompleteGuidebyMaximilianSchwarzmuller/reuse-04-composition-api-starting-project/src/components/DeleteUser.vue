abd29336a21092ca920bed91d6b46eaa43ce72c6
Piyush Sachan <piyush.fgk@gmail.com>
2024-03-28 06:42:29 +0600

    using same mixin in delete component composition API

--- Vue-TheCompleteGuidebyMaximilianSchwarzmuller/reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
+++ Vue-TheCompleteGuidebyMaximilianSchwarzmuller/reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
@@ -15,15 +15,8 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert';
 
-const alertIsVisible = ref(false);
-
-function showAlert() {
-  alertIsVisible.value = true;
-}
-function hideAlert() {
-  alertIsVisible.value = false;
-}
+const [alertIsVisible, showAlert, hideAlert] = useAlert();
 </script>
