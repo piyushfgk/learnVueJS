4985a3b67bce1e512e0dc5d674bdd492e1bcf554
Piyush Sachan <piyush.fgk@gmail.com>
2024-04-05 06:07:52 +0600

    Add syntax highlight using themes for markdown content

--- Nuxt3-Full-Stack-Complete-Guide-by-Piotr-Jura/portfolio/nuxt.config.ts
+++ Nuxt3-Full-Stack-Complete-Guide-by-Piotr-Jura/portfolio/nuxt.config.ts
@@ -5,4 +5,12 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: "",
   },
+  content: {
+    highlight: {
+      theme: {
+        default: "min-light",
+        dark: "min-dark",
+      },
+    },
+  },
 });
