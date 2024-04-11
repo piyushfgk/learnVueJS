0347733c7ec4d051a790e3960014b70af1a72d29
Piyush Sachan <piyush.fgk@gmail.com>
2024-04-11 09:45:31 +0600

    Blog document not found custom error

--- Nuxt3-Full-Stack-Complete-Guide-by-Piotr-Jura/portfolio/pages/blog/[...slug].vue
+++ Nuxt3-Full-Stack-Complete-Guide-by-Piotr-Jura/portfolio/pages/blog/[...slug].vue
@@ -2,23 +2,29 @@
   <article
     class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc"></ContentRenderer>
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post not found!</p>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc"></ContentRenderer>
+          </div>
+          <div class="col-span-2 not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Contents</div>
+              <nav>
+                <TocLinks
+                  :links="doc.body.toc.links"
+                  :active-id="activeId"
+                ></TocLinks>
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2 not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Contents</div>
-            <nav>
-              <TocLinks
-                :links="doc.body.toc.links"
-                :active-id="activeId"
-              ></TocLinks>
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
