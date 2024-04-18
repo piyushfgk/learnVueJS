3a3093aa96df30cbe8943bc5f66f5a3fb53ffd30
Piyush Sachan <piyush.fgk@gmail.com>
2024-04-18 08:58:57 +0600

    Add sitemap for better SEO

---
+++ Nuxt3-Full-Stack-Complete-Guide-by-Piotr-Jura/portfolio/nuxt.config.js
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  colorMode: {
+    classSuffix: "",
+  },
+  content: {
+    highlight: {
+      theme: {
+        default: "min-light",
+        dark: "min-dark",
+      },
+    },
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
+  },
+});
